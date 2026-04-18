34845ab19e0e692dca03ca76aa748c0fec631bca
Nassim na <nassimeloiardighi10@gmail.com>
2026-04-18 01:06:51 +0100

    ex6

---
+++ python6/alchemy/__init__.py
@@ -0,0 +1,3 @@
+from .elements import create_air
+from .potions import healing_potion as heal
+from .potions import strength_potion
---
+++ python6/elements.py
@@ -0,0 +1,4 @@
+def create_fire():
+    return "Fire element created"
+def create_water():
+    return "Water element created"
